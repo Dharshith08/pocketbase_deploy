ba4f0fd2db33e7e288e461e5586d621faa1f9113
Dharshith B <155810845+Dharshith08@users.noreply.github.com>
2026-03-23 21:48:22 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
+echo "Downloading PocketBase..."
+
+wget https://github.com/pocketbase/pocketbase/releases/download/v0.22.0/pocketbase_0.22.0_linux_amd64.zip
+
+unzip pocketbase_0.22.0_linux_amd64.zip
+
 chmod +x pocketbase
-./pocketbase serve --http=0.0.0.0:$PORT
+
+echo "Starting PocketBase..."
+
+./pocketbase serve --http=0.0.0.0:$PORT --dir=/pb_data
